7abc0281ec9d4e102a0bf6fbb23e8bf90a82da04
mina <sadeghbayan.mina@gmail.com>
2023-08-28 13:08:51 +0330

    change basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="handmade-ceramic-online-store">
         <App />
       </BrowserRouter>
     </AppProvider>
